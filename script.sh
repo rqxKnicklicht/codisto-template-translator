f89128fcf7f42a8228129f55e83c6b2c96f8af9c
rqxKnicklicht <k.externbrink@web.de>
2022-12-01 20:56:45 +0100

    updates env files

---
+++ script.sh
@@ -0,0 +1,7 @@
+export BASEDIR="/home/kexternbrink/source/codisto-template-translator"
+eval "$(cat ${BASEDIR}/languages/de.env)" && envsubst < ${BASEDIR}/template.txt > ${BASEDIR}/results/ebay-DE.liquid
+eval "$(cat ${BASEDIR}/languages/fr.env)" && envsubst < ${BASEDIR}/template.txt > ${BASEDIR}/results/ebay-FR.liquid
+eval "$(cat ${BASEDIR}/languages/it.env)" && envsubst < ${BASEDIR}/template.txt > ${BASEDIR}/results/ebay-IT.liquid
+eval "$(cat ${BASEDIR}/languages/es.env)" && envsubst < ${BASEDIR}/template.txt > ${BASEDIR}/results/ebay-ES.liquid
+eval "$(cat ${BASEDIR}/languages/ie.env)" && envsubst < ${BASEDIR}/template.txt > ${BASEDIR}/results/ebay-IE.liquid
+eval "$(cat ${BASEDIR}/languages/en.env)" && envsubst < ${BASEDIR}/template.txt > ${BASEDIR}/results/ebay-EN.liquid
